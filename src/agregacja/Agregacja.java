b35195a7eda8bd9be9b54d8b2027a8c9bf014df8
JaroslawPartyka <partyka.jaroslaw@gmail.com>
2020-02-20 14:37:33 +0100

    refactored

--- src/agregacja/Agregacja.java
+++ src/agregacja/Agregacja.java
@@ -2,11 +2,36 @@ package agregacja;
 
 public class Agregacja {
     public static void main(String[] args) {
-
+        Pracownik p = new Pracownik("Andrzej", new Adres("Szkolna", 1));
+        System.out.println(p);
     }
 }
 
 class Pracownik {
     String imie;
+    Adres adres;
 
+    public Pracownik(String imie, Adres adres) {
+        this.imie = imie;
+        this.adres = adres;
+    }
+
+    @Override
+    public String toString() {
+        return "Pracownik{" +
+                "imie='" + imie + '\'' +
+                ", adres=" + adres +
+                '}';
+    }
+}
+
+class Adres {
+    String ulica;
+
+    public Adres(String ulica, int nrDomu) {
+        this.ulica = ulica;
+        this.nrDomu = nrDomu;
+    }
+
+    int nrDomu;
 }
